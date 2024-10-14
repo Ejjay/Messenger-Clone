516f6cc5efb62c7432f592352e014def1519b958
Ejjay <ejalloso021@gmail.com>
2024-10-14 18:08:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      swcPlugins: [["next-superjson-plugin", {}]]
-    },
-    images: {
-      domains: [
-        'res.cloudinary.com', 
-        'avatars.githubusercontent.com',
-        'lh3.googleusercontent.com'
-      ]
-    }
-  }
-  
-  module.exports = nextConfig
+  experimental: {
+    appDir: true,
+    swcPlugins: [["next-superjson-plugin", {}]],
+    runtime: 'nodejs', // Disable Edge Runtime
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
